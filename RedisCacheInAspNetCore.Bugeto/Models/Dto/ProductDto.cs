eb9a38692f4b5041322089703c154cf159193d15
Behradnafar <behrdnafar@gmail.com>
2021-09-22 11:59:32 -0400

    Add project files.

---
+++ RedisCacheInAspNetCore.Bugeto/Models/Dto/ProductDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisCacheInAspNetCore.Bugeto.Models.Dto
+{
+    public class ProductDto
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+    }
+
+
+    public class HomePageDto
+    {
+        public LastProduct LastProduct { get; set; }
+        public BestProduct  BestProduct { get; set; }
+    }
+
+    public class LastProduct
+    {
+        public List<ProductDto> Products { get; set; }
+    }
+
+    public class BestProduct
+    {
+        public List<ProductDto> Products { get; set; }
+
+    }
+}
---
+++ RedisCacheInAspNetCore.Bugeto/Models/ProductRepository.cs
@@ -0,0 +1,41 @@
+﻿using RedisCacheInAspNetCore.Bugeto.Models.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RedisCacheInAspNetCore.Bugeto.Models
+{
+    public interface IProductRepository
+    {
+        HomePageDto GetHomePageProducts();
+    }
+    public class ProductRepository: IProductRepository
+    {
+        public HomePageDto GetHomePageProducts()
+        {
+            Thread.Sleep(3000);
+            return new HomePageDto()
+            {
+                 BestProduct = new BestProduct()
+                {
+                    Products = new List<ProductDto>()
+                      {
+                           new ProductDto { Id=1, Name="Solid Tutorial"},
+                           new ProductDto { Id=2, Name="Design pattern Tutorial"}
+                      }
+                },
+                 LastProduct = new LastProduct()
+                {
+                    Products = new List<ProductDto>()
+                      {
+                           new ProductDto { Id=3, Name="Microservices"},
+                           new ProductDto { Id=4, Name="DDD"},
+                           new ProductDto { Id=5, Name="C#"}
+                      }
+                }
+            };
+        }
+    }
+}
